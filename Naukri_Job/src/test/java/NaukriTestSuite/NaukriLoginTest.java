2ca054acbf7577d41530bf2e7007132028fcad12
kiranvanteru <kk23vanteru@gmail.com>
2017-04-06 20:22:31 +0530

    changes by Kiran

--- Naukri_Job/src/test/java/NaukriTestSuite/NaukriLoginTest.java
+++ Naukri_Job/src/test/java/NaukriTestSuite/NaukriLoginTest.java
@@ -23,7 +23,6 @@ public class NaukriLoginTest {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://www.naukri.com/");
-		System.out.println("Sending to git");
 	  }
 	
 	
